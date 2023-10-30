a10003a3ebeec78d0fb697107c8c10e93dc84e65
Dheeraj Pyasi <dheerajpyasi101@gmail.com>
2023-10-31 01:42:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'cwh-todo-list';
+  // constructor(){
+  //   setTimeout(()=>
+  //   {
+  //     this.title="changed title";
+
+  //   },2000);
+  // }
 }
